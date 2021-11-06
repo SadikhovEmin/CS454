8c8de870ea806943d04d95ae8f1ebb0768e0e5e5
SadikhovEmin <eemin.sadykhov@gmail.com>
2021-11-06 14:59:22 +0300

    Finished Part 1 of HW

--- HW2/main.py
+++ HW2/main.py
@@ -1,4 +1,6 @@
 from csv import reader
+import math
+import matplotlib.pyplot as plt
 
 
 def mean(data):
@@ -11,20 +13,23 @@ def mean(data):
     return values / count
 
 
+''' VARIABLES '''
+training_csv = None
+petal_length_setosa, petal_width_setosa = [], []
+petal_length_versicolor, petal_width_versicolor = [], []
+petal_length_virginica, petal_width_virginica = [], []
+
+''' MEANS '''
+petal_length_setosa_mean, petal_width_setosa_mean = 0, 0
+petal_length_versicolor_mean, petal_width_versicolor_mean = 0, 0
+petal_length_virginica_mean, petal_width_virginica_mean = 0, 0
+
 with open('training.csv') as training_file:
     csv_reader = reader(training_file)
     training_csv = list(csv_reader)
-    # print(training_csv)
     training_csv.pop(0)
-    # print(training_csv)
-
-    petal_length_setosa, petal_width_setosa = [], []
-    petal_length_versicolor, petal_width_versicolor = [], []
-    petal_length_virginica, petal_width_virginica = [], []
 
     for i in training_csv:
-        # print(i[2])
-
         if i[2] == "Iris-setosa":
             petal_length_setosa.append(float(i[0]))
             petal_width_setosa.append(float(i[1]))
@@ -39,22 +44,88 @@ with open('training.csv') as training_file:
     petal_length_setosa_mean = mean(petal_length_setosa)
     petal_width_setosa_mean = mean(petal_width_setosa)
 
-    print('Length : ', petal_length_setosa_mean, 'Width : ', petal_width_setosa_mean)
-
     ''' VERSICOLOR '''
     petal_length_versicolor_mean = mean(petal_length_versicolor)
     petal_width_versicolor_mean = mean(petal_width_versicolor)
-
-    print('Length : ', petal_length_versicolor_mean, 'Width : ', petal_width_versicolor_mean)
 
     ''' VIRGINICA '''
     petal_length_virginica_mean = mean(petal_length_virginica)
     petal_width_virginica_mean = mean(petal_width_virginica)
 
-    print('Length : ', petal_length_virginica_mean, 'Width : ', petal_width_virginica_mean)
+    plt.scatter(list(petal_length_setosa), list(petal_width_setosa), color='red', marker='x')
+    plt.scatter(list(petal_length_versicolor), list(petal_width_versicolor), color='blue', marker='x')
+    plt.scatter(list(petal_length_virginica), list(petal_width_virginica), color='green', marker='x')
+    plt.scatter(petal_length_setosa_mean, petal_width_setosa_mean, color='black')
+    plt.scatter(petal_length_versicolor_mean, petal_width_versicolor_mean, color='gray')
+    plt.scatter(petal_length_virginica_mean, petal_width_virginica_mean, color='brown')
+    plt.legend(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica', 'Iris-setosa Mean', 'Iris-versicolor Mean',
+                'Iris-virginica Mean'])
+
+    plt.show()
+
+with open('testing.csv') as testing_file:
+    csv_reader = reader(testing_file)
+    testing_csv = list(csv_reader)
+    testing_csv.pop(0)
 
     ''' Confusion matrix '''
+    confusion_matrix = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
 
-#
-# if __name__ == '__main__':
-#     pass
+    for i in testing_csv:
+        euclidean_setosa = math.sqrt(
+            math.pow((float(i[0]) - petal_length_setosa_mean), 2) + math.pow((float(i[1]) - petal_width_setosa_mean),
+                                                                             2))
+        euclidean_versicolor = math.sqrt(math.pow((float(i[0]) - petal_length_versicolor_mean), 2) + math.pow(
+            (float(i[1]) - petal_width_versicolor_mean), 2))
+        euclidean_virginca = math.sqrt(math.pow((float(i[0]) - petal_length_virginica_mean), 2) + math.pow(
+            (float(i[1]) - petal_width_virginica_mean), 2))
+
+        prediction = min(euclidean_setosa, euclidean_versicolor, euclidean_virginca)  # Gets the closest point
+
+        if prediction == euclidean_setosa:
+            if i[2] == "Iris-setosa":
+                confusion_matrix[0][0] += 1
+            if i[2] == "Iris-versicolor":
+                confusion_matrix[0][1] += 1
+            if i[2] == "Iris-virginica":
+                confusion_matrix[0][2] += 1
+        elif prediction == euclidean_versicolor:
+            if i[2] == "Iris-setosa":
+                confusion_matrix[1][0] += 1
+            if i[2] == "Iris-versicolor":
+                confusion_matrix[1][1] += 1
+            if i[2] == "Iris-virginica":
+                confusion_matrix[1][2] += 1
+        elif prediction == euclidean_virginca:
+            if i[2] == "Iris-setosa":
+                confusion_matrix[2][0] += 1
+            if i[2] == "Iris-versicolor":
+                confusion_matrix[2][1] += 1
+            if i[2] == "Iris-virginica":
+                confusion_matrix[2][2] += 1
+
+    print(*confusion_matrix, sep='\n')
+    print(training_csv)
+
+    ''' Part 2 '''
+    list_euclidians = []
+
+    for i in testing_csv:
+        temp_results = []
+
+        for j in training_csv:
+            temp_results.append(math.sqrt(
+                math.pow((float(i[0]) - float(j[0])), 2) + math.pow((float(i[1]) - float(j[1])),
+                                                                    2)))
+        list_euclidians.append(temp_results)
+
+    print('list euclidians ', list_euclidians)
+
+if __name__ == '__main__':
+    print('Length : ', petal_length_setosa_mean, 'Width : ', petal_width_setosa_mean)
+    print('Length : ', petal_length_versicolor_mean, 'Width : ', petal_width_versicolor_mean)
+    print('Length : ', petal_length_virginica_mean, 'Width : ', petal_width_virginica_mean)
