028cec6e97cd9931df7a75be5e803395e38515d3
SadikhovEmin <eemin.sadykhov@gmail.com>
2021-11-07 17:15:21 +0300

    Temp not finished

--- HW2/main.py
+++ HW2/main.py
@@ -15,6 +15,7 @@ def mean(data):
 
 ''' VARIABLES '''
 training_csv = None
+testing_csv = None
 petal_length_setosa, petal_width_setosa = [], []
 petal_length_versicolor, petal_width_versicolor = [], []
 petal_length_virginica, petal_width_virginica = [], []
@@ -109,21 +110,85 @@ with open('testing.csv') as testing_file:
                 confusion_matrix[2][2] += 1
 
     print(*confusion_matrix, sep='\n')
-    print(training_csv)
+    # print(training_csv)
 
     ''' Part 2 '''
-    list_euclidians = []
+    list_euclidean = []
 
     for i in testing_csv:
         temp_results = []
 
         for j in training_csv:
-            temp_results.append(math.sqrt(
+            temp_results.append([math.sqrt(
                 math.pow((float(i[0]) - float(j[0])), 2) + math.pow((float(i[1]) - float(j[1])),
-                                                                    2)))
-        list_euclidians.append(temp_results)
+                                                                    2)), training_csv.index(j)])
+        # print(sorted(temp_results))
+        list_euclidean.append(sorted(temp_results))  # Sorting all of the euclidean
 
-    print('list euclidians ', list_euclidians)
+    # Need to calculate count of them for k = 1, 3, 5, 7, 9 and plot them
+    # First creating a confusion matrix
+    # confusion_matrix = [
+    #     [0, 0, 0],
+    #     [0, 0, 0],
+    #     [0, 0, 0]
+    # ]
+
+    print(testing_csv[0])
+    print(list_euclidean[0], sep='\n')
+    count, k = 0, 1
+    '''
+    i = testingdeki bir instance
+    list_euclidean[index(i)]
+    
+    '''
+    print(len(list_euclidean[0]))
+    for i in list_euclidean:
+
+        setosa, versicolor, virginica = 0, 0, 0
+        confusion_matrix = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        print(i)
+
+        for j in range(0, k):
+            if list_euclidean[j][2] == "Iris-setosa":
+                setosa += 1
+            elif list_euclidean[j][2] == "Iris-versicolor":
+                versicolor += 1
+            elif list_euclidean[j][2] == "Iris-virginica":
+                virginica += 1
+
+        for z in testing_csv:
+            if virginica > setosa and virginica > versicolor:
+                if z[2] == "Iris-setosa":
+                    confusion_matrix[2][0] += 1
+                if z[2] == "Iris-versicolor":
+                    confusion_matrix[2][1] += 1
+                if z[2] == "Iris-virginica":
+                    confusion_matrix[2][2] += 1
+            elif setosa > versicolor and setosa > virginica:
+                if z[2] == "Iris-setosa":
+                    confusion_matrix[0][0] += 1
+                if z[2] == "Iris-versicolor":
+                    confusion_matrix[0][1] += 1
+                if z[2] == "Iris-virginica":
+                    confusion_matrix[0][2] += 1
+            elif versicolor > setosa and versicolor > virginica:
+                if z[2] == "Iris-setosa":
+                    confusion_matrix[1][0] += 1
+                if z[2] == "Iris-versicolor":
+                    confusion_matrix[1][1] += 1
+                if z[2] == "Iris-virginica":
+                    confusion_matrix[1][2] += 1
+
+        print("Confusion matrix k = ", k)
+        print(*confusion_matrix, sep='\n')
+
+        k += 2
+        if k == 11:
+            break
 
 if __name__ == '__main__':
     print('Length : ', petal_length_setosa_mean, 'Width : ', petal_width_setosa_mean)
